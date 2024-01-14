5eb7bf979dcdabe7edcb9b4bc752af76612a3876
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2024-01-14 23:11:43 +0900

    디렉토리 구조

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import type { ReactNode } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -10,13 +11,9 @@ export const metadata: Metadata = {
   title: 'Create Next App',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <body className={inter.className}>{children}</body>
     </html>
   );
