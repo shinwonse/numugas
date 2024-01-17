f93ed3604ec967aae27ce0c9ba8d222fa2aafcf4
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2024-01-18 00:07:01 +0900

    path alias 이슈 해결

--- packages/eslint-config/react-internal.js
+++ packages/eslint-config/react-internal.js
@@ -1,7 +1,7 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   extends: ["@wonse/eslint-config", "eslint-config-turbo"],
-  ignorePatterns: [".eslintrc"],
+  ignorePatterns: [".eslintrc.js"],
   rules: {
     "no-restricted-exports": "off",
     "@typescript-eslint/consistent-type-imports": "error"
