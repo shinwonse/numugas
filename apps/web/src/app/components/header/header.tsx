dc3d4ad2bf02800c0d4a1bc9e4c317c7ac74ed4c
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2024-01-28 16:51:25 +0900

    헤더 레이아웃 수정

--- apps/web/src/app/components/header/header.tsx
+++ apps/web/src/app/components/header/header.tsx
@@ -1,3 +1,4 @@
+import { HamburgerMenuIcon, HomeIcon } from '@radix-ui/react-icons';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -9,15 +10,23 @@ function Header() {
   return (
     <header
       className={cn(
-        'flex flex-row items-center justify-center py-6 bg-accent rounded-b-2xl md:py-8'
+        'flex flex-row justify-between py-4 bg-accent rounded-b-2xl px-4'
       )}
     >
+      <button type="button">
+        <HamburgerMenuIcon color="white" height={24} width={24} />
+      </button>
       <Link
-        className={cn('h-20 w-20 absolute top-0.5 md:w-28 md:h-28')}
+        className={cn(
+          'left-1/2 transform -translate-x-1/2 h-20 w-20 absolute top-0.5 md:w-28 md:h-28'
+        )}
         href="/"
       >
         <Image alt="담장 로고" fill src={Logo} />
       </Link>
+      <button type="button">
+        <HomeIcon color="white" height={24} width={24} />
+      </button>
     </header>
   );
 }
